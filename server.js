e3ad84cdd84139938b09fc79b7714fa27d55a7b5
Kuluni2002 <gamagekuluni@gmail.com>
2025-10-05 19:27:10 +0530

    Integrate authentication routes into Express server

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ const app = express();
 app.use(cors()); // Allow requests from anywhere
 app.use(express.json()); // Understand JSON data
 
+// Import routes
+const authRoutes = require('./src/routes/authRoutes');
+
+// Mount routes
+app.use('/api/auth', authRoutes);
+
 // Test route - this is your first API endpoint!
 app.get('/', (req, res) => {
   res.json({ 
